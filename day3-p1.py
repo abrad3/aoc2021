bc91098b787e0582b4e4cd1a49246d9e7a902dee
abrad3 <apollo@redhat.com>
2021-12-03 18:36:15 +1100

    added comments so future apollo understands

--- day3-p1.py
+++ day3-p1.py
@@ -3,7 +3,12 @@ filename="./input-day3.csv"
 df=pd.read_csv(filename, header=None, dtype=str)
 df.columns=['data']
 ls=df['data'].to_list()
+
+# initialising binary gamma, which I represent with a str
 bgamma=""
+
+# two for loops, one to go over each bit
+# and an inner one to go through each piece of data
 for i in range(0, len(ls[0])):
     counter=0
     for j in range(0, len(ls)):
@@ -11,7 +16,11 @@ for i in range(0, len(ls[0])):
     if counter >= len(ls)//2: bgamma+="1"
     else: bgamma+="0"
 
+# find the binary value of epsilon by inverting the binary gamma value
 bepsilon = "".join(["1" if b == "0" else "0" for b in bgamma])
+
+# convert binary values to base 10
 gamma=int(bgamma,2)
 epsilon=int(bepsilon,2)
+
 print("the power consumption is: "+str(gamma*epsilon))    
